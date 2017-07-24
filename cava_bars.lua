fb4ac4c332d04680a05ed44714304145bf89f587
Michael Sorenson <m.sorenson407@gmail.com>
2017-07-23 20:01:43 -0600

    Updates to colors.

--- cava_bars.lua
+++ cava_bars.lua
@@ -20,32 +20,31 @@ function conky_main()
     local cr = cairo_create(cs)
 
     -- settings
-    local fill_red=0.15
-    local fill_green=.47
-    local fill_blue=.67
-    local fill_alpha=1
-    local line_red=.35
-    local line_green=.16
-    local line_blue=.81
-    local line_alpha=.8
+    local bar_red=0.13
+    local bar_green=.586
+    local bar_blue=.95
+    local bar_alpha=.7
+
+    local shadow_red=.1
+    local shadow_green=.1
+    local shadow_blue=.1
+    local shadow_alpha=.4
 
     local line_width=5
 
     local x_offset = 135
     for height in string.gmatch(new_text, "%d+") do
       local top_left_x = x_offset
-      local top_left_y = 500 - (height / 2)
+      local top_left_y = 550 - (height / 2)
       local rec_width = 50
       local rec_height = height
-      -- cairo_set_line_width (cr,line_width)
-      cairo_rectangle (cr,top_left_x + 10,top_left_y + 10,rec_width,rec_height)
-      cairo_set_source_rgba (cr,line_red,line_green,line_blue,line_alpha)
-      cairo_fill (cr)
-      cairo_rectangle (cr,top_left_x,top_left_y,rec_width,rec_height)
-      cairo_set_source_rgba (cr,fill_red,fill_green,fill_blue,fill_alpha)
-      cairo_fill (cr)
-      -- cairo_set_source_rgba (cr,line_red,line_green,line_blue,line_alpha)
-      -- cairo_stroke (cr)
+
+      cairo_rectangle(cr,top_left_x + 10,top_left_y + 10,rec_width,rec_height)
+      cairo_set_source_rgba(cr, shadow_red, shadow_green, shadow_blue, shadow_alpha)
+      cairo_fill(cr)
+      cairo_rectangle(cr, top_left_x, top_left_y, rec_width, rec_height)
+      cairo_set_source_rgba(cr, bar_red, bar_green, bar_blue, bar_alpha)
+      cairo_fill(cr)
 
       x_offset = x_offset + 75
     end
